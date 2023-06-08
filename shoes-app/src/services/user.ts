41c58540aacc1cfde6a4490ce64f6f6fea0a0da8
Ewen35550 <ewen.herve.35550@gmail.com>
2023-06-08 09:06:28 +0200

    feat(user.ts) ajout des fonctions pour le panier

--- shoes-app/src/services/user.ts
+++ shoes-app/src/services/user.ts
@@ -1,5 +1,5 @@
 import { IError } from "../interfaces/error.interface";
-import { IShoes } from "../interfaces/shoes.interface";
+import { IShoes, IShoesCart } from "../interfaces/shoes.interface";
 import axiosInstance from "../utils/axiosInstance";
 
 export async function addToFavorite(
@@ -40,6 +40,62 @@ export async function getFavorites(): Promise<{
 }> {
   try {
     const res = await axiosInstance.get(`/favorite`);
+    return { data: res.data };
+  } catch (e: any) {
+    return {
+      error: {
+        message: e.response.data.message,
+        status: e.response.status,
+      },
+    };
+  }
+}
+
+export async function addToCart(
+  id: string,
+  quantity: number,
+  size: number
+): Promise<{ data?: IShoesCart; error?: IError }> {
+  try {
+    const res = await axiosInstance.post(`/cart/${id}`, {
+      quantity,
+      size,
+    });
+
+    return { data: res.data };
+  } catch (e: any) {
+    return {
+      error: {
+        message: e.response.data.message,
+        status: e.response.status,
+      },
+    };
+  }
+}
+
+export async function getCart(): Promise<{
+  data?: IShoesCart[];
+  error?: IError;
+}> {
+  try {
+    const res = await axiosInstance.get(`/cart`);
+    return { data: res.data };
+  } catch (e: any) {
+    return {
+      error: {
+        message: e.response.data.message,
+        status: e.response.status,
+      },
+    };
+  }
+}
+
+export async function deleteUser(id: string): Promise<{
+  data?: string;
+  error?: IError;
+}> {
+  try {
+    const res = await axiosInstance.delete(`/user/${id}`);
     return { data: res.data };
   } catch (e: any) {
     return {
